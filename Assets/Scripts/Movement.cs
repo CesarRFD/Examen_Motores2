6609378793318b60e7d150ed826334f7b9601589
CesarRFD <133169415+CesarRFD@users.noreply.github.com>
2023-10-06 00:39:32 -0600

    Si

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] private Animator anim;
     [SerializeField] private float horizontal;
     [SerializeField] private float vertical;
     [SerializeField] private float speed = 3;
+    [SerializeField] private GameObject sword;
 
-    private Rigidbody2D RB;
+    float newPosX = 1f;
+    float newPosY = -0.15f;
 
     private void Awake()
     {
@@ -17,14 +20,73 @@ public class Movement : MonoBehaviour
 
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        //sword = GameObject.Find("Sword");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            StartCoroutine(Attack(.1f));
+        }
+
+        GameObject targetObject = GameObject.FindWithTag("Sword");
+
+        Transform playerPos = GameObject.Find("Player").transform;
+        Transform swordPos = GameObject.Find("Sword").transform;
+        Vector3 relativePosition = playerPos.InverseTransformPoint(swordPos.position);
+
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
         transform.Translate(horizontal * speed * Time.deltaTime, vertical * speed * Time.deltaTime, 0);
+
+        if (horizontal>0)
+        {
+            Debug.Log("derecha");
+            anim.SetFloat("IdleX", 1f);
+            anim.SetFloat("IdleY", 0f);
+            newPosX = 1f;
+            newPosY = -0.15f;
+            swordPos.localPosition = new Vector3(newPosX, newPosY, relativePosition.z);
+            targetObject.transform.rotation = Quaternion.Euler(0f, 0f, 180f);
+        }
+        if (horizontal < 0)
+        {
+            Debug.Log("izquierda");
+            anim.SetFloat("IdleX", -1f);
+            anim.SetFloat("IdleY", 0f);
+            newPosX = -1f;
+            newPosY = -0.15f;
+            swordPos.localPosition = new Vector3(newPosX, newPosY, relativePosition.z);
+            targetObject.transform.rotation = Quaternion.Euler(0f, 0f, 180f);
+        }
+        if (vertical > 0)
+        {
+            Debug.Log("arriba");
+            anim.SetFloat("IdleX", 0f);
+            anim.SetFloat("IdleY", 1f);
+            newPosX = 0f;
+            newPosY = .8f;
+            swordPos.localPosition = new Vector3(newPosX, newPosY, relativePosition.z);
+            targetObject.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+        }
+        if (vertical < 0)
+        {
+            Debug.Log("abajo");
+            anim.SetFloat("IdleX", 0f);
+            anim.SetFloat("IdleY", -1f);
+            newPosX = 0f;
+            newPosY = -0.8f;
+            swordPos.localPosition = new Vector3(newPosX, newPosY, relativePosition.z);
+            targetObject.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+        }
+    }
+    IEnumerator Attack(float Tiempo)
+    {
+        sword.SetActive(true);
+        yield return new WaitForSeconds(Tiempo);
+        sword.SetActive(false);
     }
 }
